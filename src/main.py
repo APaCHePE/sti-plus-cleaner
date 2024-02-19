5df29641a79f8e004953f80718a65b5fb4a6328c
Apachepe <alexanderspradochoquepata@gmail.com>
2024-02-19 13:25:13 -0500

    mover archivos

--- src/main.py
+++ src/main.py
@@ -1,24 +1,18 @@
-import socket
 import os
+import sys
+from dotenv import load_dotenv
+sys.path.append('app')
+from app.service import UsersService
+# from modules.logging.logging_default import logger
 
-try:
-    host = os.environ["HOST_IP"]
-except KeyError:
-    host = "0.0.0.0"
+load_dotenv()
 
-try:
-    port = int(os.environ["HOST_PORT"])
-except KeyError:
-    port = 22
-
-print("Host Origen: {}".format(socket.gethostbyname(socket.gethostname())))
-
-timeout_seconds = 1
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.settimeout(timeout_seconds)
-result = sock.connect_ex((host, int(port)))
-if result == 0:
-    print("Host Destino: {}, Port: {} - True".format(host, port))
-else:
-    print("Host Destino: {}, Port: {} - False".format(host, port))
-sock.close()
+def main():
+    print("Iniciando captura de datos... v1.0.1")
+    
+    user = os.getenv('USER')
+    process = os.getenv('PROCESS')
+    print(f"USER->{user}|PROCESS->{process}")
+    UsersService.cleaner(user, process)
+if __name__ == "__main__":
+    main()
